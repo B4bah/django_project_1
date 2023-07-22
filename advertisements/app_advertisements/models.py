df94604f34c02683877f3c209a640f58261cc492
B4bah <osipovmisha0909@yandex.ru>
2023-07-22 16:34:22 +0300

    lesson_7 work

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisements(models.Model):
+    title = models.CharField(
+        verbose_name='Title',
+        help_text='Field for products name',
+        max_length=100
+    )
+    description = models.TextField(
+        verbose_name='Description'
+    )
+    price = models.DecimalField(
+        verbose_name='Price',
+        max_digits=10,
+        decimal_places=2,
+    )
+    auction = models.BooleanField(
+        verbose_name='Sale',
+        default=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
