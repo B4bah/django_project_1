3cd6f7cef2c111478fe24ca84e61bd198cb66f31
B4bah <osipovmisha0909@yandex.ru>
2023-08-05 15:01:59 +0300

    Homework lesson_8

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'auction']
+    list_filter = ['auction', 'created_at']
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+    fieldsets = (
+        ('General', {
+            'fields': ('title', 'description')
+            }
+         ),
+        ('Finances', {
+            'fields': ('price', 'auction'),
+            'classes': ['collapse']
+            }
+         )
+    )
+
+    @admin.action(description='Remove the possibility of sale')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+
+    @admin.action(description='Add the possibility of sale')
+    def make_auction_ad_true(self, request, queryset):
+        queryset.update(auction=True)
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
 
 
-class Advertisements(models.Model):
-
+class Advertisement(models.Model):
     title = models.CharField(
         verbose_name='Title',
-        help_text='Field for products name',
+        help_text='Field for product name',
         max_length=100
     )
     description = models.TextField(
@@ -23,9 +25,28 @@ class Advertisements(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    @admin.display(description='Creation date')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strf.time('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Today at {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y at %H:%M:%S")
+
+    @admin.display(description='Last updated')
+    def updeted_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: red; font-weight: bold;">Today at {}</span>', updated_time
+            )
+        return self.updated_at.strftime("%d.%m.%Y at %H:%M:%S")
+
     def __str__(self):
-        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price:.2f})>'
+        return f'<id={self.id} title={self.title} price={self.price:.2f}>'
 
-
-class Meta:
-    db_table = 'advertisements'
+    class Meta:
+        db_table = 'advertisement'
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
