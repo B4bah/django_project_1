7fbebd2f531e6188c57846d0c92c95a85b051632
B4bah <osipovmisha0909@yandex.ru>
2023-08-24 00:31:29 +0300

    Lesson 9 homework

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -2,23 +2,19 @@ from django.contrib import admin
 from .models import Advertisement
 
 
-
-
-
-
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction', Advertisement.Show_image]
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction', 'show_image']
     list_filter = ['auction', 'created_at']
     actions = ['make_auction_as_false', 'make_auction_as_true']
     fieldsets = (
         ('General', {
             'fields': ('title', 'description', 'user', 'image')
-            }
+        }
          ),
         ('Finances', {
             'fields': ('price', 'auction'),
             'classes': ['collapse']
-            }
+        }
          )
     )
 
--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -9,63 +9,141 @@ User = get_user_model()
 
 class Advertisement(models.Model):
     image = models.ImageField(
-        verbose_name='Image',
-        upload_to='advertisements/'
+        verbose_name="Image",
+        upload_to="advertisements/"
     )
 
     user = models.ForeignKey(
-        to=User,
+        User,
         on_delete=models.CASCADE,
-        verbose_name='User'
+        verbose_name="User"
     )
 
     title = models.CharField(
-        verbose_name='Title',
-        help_text='Field for product name',
-        max_length=100
+        verbose_name="Name",
+        help_text="Field for product name",
+        max_length=100,
     )
     description = models.TextField(
-        verbose_name='Description'
+        verbose_name="Description"
     )
     price = models.DecimalField(
-        verbose_name='Price',
+        verbose_name="Price",
         max_digits=10,
         decimal_places=2,
     )
     auction = models.BooleanField(
-        verbose_name='Sale',
+        verbose_name="Auction",
         default=False
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    @admin.display(description='Creation date')
+    @admin.display(description="Creation date")
     def created_date(self):
         if self.created_at.date() == timezone.now().date():
-            created_time = self.created_at.time().strftime('%H:%M:%S')
+            created_time = self.created_at.time().strftime("%H:%M:%S")
             return format_html(
                 '<span style="color: green; font-weight: bold;">Today at {}</span>', created_time
             )
-        return self.created_at.strftime("%d.%m.%Y at %H:%M:%S")
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
 
-    @admin.display(description='Last updated')
+    @admin.display(description="Last updated")
     def updated_date(self):
         if self.updated_at.date() == timezone.now().date():
-            updated_time = self.updated_at.strftime('%H:%M:%S')
+            created_time = self.updated_at.time().strftime("%H:%M:%S")
             return format_html(
-                '<span style="color: red; font-weight: bold;">Today at {}</span>', updated_time
+                '<span style="color: blue; font-weight: bold">Today at {}</span>', created_time
             )
-        return self.updated_at.strftime("%d.%m.%Y at %H:%M:%S")
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
 
-    @admin.display(description='Show image')
-    def Show_image(self):
+    @admin.display(description="Image diplay")
+    def show_image(self):
         if self.image:
-            return format_html('<img scr={} style="width: 50px; height: 50px">', self.image.url)
+            return format_html(
+                '<img src={} style="width: 50px; height: 50px">', self.image.url
+            )
 
     def __str__(self):
-        return f'<id={self.id} title={self.title} price={self.price:.2f}>'
+        return f"id = {self.id} title = {self.title} price = {self.price}"
 
     class Meta:
         db_table = 'advertisement'
-        verbose_name = 'Product'
-        verbose_name_plural = 'Products'
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+
+
+
+
+
+# from django.db import models
+# from django.contrib import admin
+# from django.utils import timezone
+# from django.utils.html import format_html
+# from django.contrib.auth import get_user_model
+#
+# User = get_user_model()
+#
+#
+# class Advertisement(models.Model):
+#     image = models.ImageField(
+#         verbose_name='Image',
+#         upload_to='advertisements/'
+#     )
+#
+#     user = models.ForeignKey(
+#         User,
+#         on_delete=models.CASCADE,
+#         verbose_name='User'
+#     )
+#
+#     title = models.CharField(
+#         verbose_name='Title',
+#         help_text='Field for product name',
+#         max_length=100
+#     )
+#     description = models.TextField(
+#         verbose_name='Description'
+#     )
+#     price = models.DecimalField(
+#         verbose_name='Price',
+#         max_digits=10,
+#         decimal_places=2,
+#     )
+#     auction = models.BooleanField(
+#         verbose_name='Sale',
+#         default=False
+#     )
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#
+#     @admin.display(description='Creation date')
+#     def created_date(self):
+#         if self.created_at.date() == timezone.now().date():
+#             created_time = self.created_at.time().strftime('%H:%M:%S')
+#             return format_html(
+#                 '<span style="color: green; font-weight: bold;">Today at {}</span>', created_time
+#             )
+#         return self.created_at.strftime("%d.%m.%Y at %H:%M:%S")
+#
+#     @admin.display(description='Last updated')
+#     def updated_date(self):
+#         if self.updated_at.date() == timezone.now().date():
+#             updated_time = self.updated_at.strftime('%H:%M:%S')
+#             return format_html(
+#                 '<span style="color: red; font-weight: bold;">Today at {}</span>', updated_time
+#             )
+#         return self.updated_at.strftime("%d.%m.%Y at %H:%M:%S")
+#
+#     @admin.display(description='Show image')
+#     def Show_image(self):
+#         if self.image:
+#             return format_html('<img scr={} style="width: 50px; height: 50px">', self.image.url)
+#
+#     def __str__(self):
+#         return f'<id={self.id} title={self.title} price={self.price:.2f}>'
+#
+#     class Meta:
+#         db_table = 'advertisement'
+#         verbose_name = 'Product'
+#         verbose_name_plural = 'Products'
