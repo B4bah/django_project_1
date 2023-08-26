8be8a44c428235b0a083983c7754cea9ad783e68
B4bah <osipovmisha0909@yandex.ru>
2023-08-26 15:06:35 +0300

    Lesson 10 homework [15.06]

--- advertisements/app_advertisements/forms.py
+++ advertisements/app_advertisements/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Advertisement
 
 
-class AdvertisementForm(forms.Form):
+class AdvertisementForm(forms.ModelForm):
     class Meta:
         model = Advertisement
         fields = ['title', 'description', 'price', 'auction', 'image']
